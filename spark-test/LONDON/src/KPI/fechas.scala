fdfedd6181a4464010b8452d580c0dc389f9624b
Luis Martin Grados Salinas <lgradoss@usersad.everis.int>
2017-09-25 18:00:21 -0500

    KPI Varios

---
+++ spark-test/LONDON/src/KPI/fechas.scala
@@ -0,0 +1,53 @@
+package KPI
+
+object fechas {
+  
+  
+  def calculoFecha (fecha:String,num:Int) = {
+    
+    //val fec1 = fecha
+    val ano = fecha.substring(0,4)
+    val mes = fecha.substring(4,6).toInt
+
+    val t = ano.toString()+mes.toString()
+    
+    //val x = mes-num 
+    
+    //var x = 
+    /*
+    for(mes <- num ){
+      
+
+    }
+    * */
+    
+    
+    /*
+    
+    if (x <=0) {
+      println(x)
+      var y = 12 - x
+      if (y 
+    } else {
+      println(x)
+    }
+    * */
+
+    
+  }
+  
+  
+  
+  def main(args: Array[String]) {
+    
+    calculoFecha("201707",8)
+    
+    //Tres meses -> 201705
+    //Seis meses -> 2017
+    
+    
+  }
+    
+
+  
+}
---
+++ spark-test/LONDON/src/KPI/helloWord.scala
@@ -0,0 +1,13 @@
+package KPI
+
+
+object helloWord {
+ 
+  def main(args: Array[String]) {
+    
+    //ClientesRecurrentes.kpi_clientes_recurrentes(ruta, esta, inicio, rangoVisitas)
+    
+    
+  }
+  
+}
